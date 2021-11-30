b754340838c1d223262fffb435ab1ef2d419768d
Anne <annekevinanguen091@gmail.com>
2021-11-30 13:36:32 +0100

    update

--- build/static/js/42.9491df10.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpgreen=this.webpackJsonpgreen||[]).push([[42],{674:function(e,n){}}]);
-//# sourceMappingURL=42.9491df10.chunk.js.map
---
+++ deploy_ubuntu.bat
@@ -0,0 +1,15 @@
+echo "deploy process start..."
+echo "removing build file in green_deploy..."
+rm -r build
+cd ../mygreen
+echo "removing build file in mygreen..."
+rm -r build
+echo "building mygreen..."
+npm run build
+echo "copying build..."
+cp -R build ../green_deploy/
+cd ../green_deploy
+echo "deploying on heroku..."
+git add .
+git commit -m "update"
+git push heroku master
